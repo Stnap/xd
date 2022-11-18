9c9efa31c232cd3ea7a33747ef3a24fcda35fc22
Stnap <ivo.larenas@alumnos.ucn.cl>
2022-11-18 09:56:42 -0300

    xd

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* Instalacion de dependencias
+yarn add react-native-countdown-circle-timer
+npm install react-native-svg
 
-export default function App() {
+yarn add react-native-ticker
+yarn add react-native-reanimated || expo install react-native-reanimated
+*/
+
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, Text, StyleSheet, View, FlatList, Alert } from "react-native";
+import Header from "./components/Header";
+import Timer from "./components/Timer";
+
+const App = () => {
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const [key, setKey] = React.useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Header title="Tiempo de espera" />
+        <Timer isPlaying={isPlaying} key={key} />
+        <Button
+          title="Pausar / Reanudar"
+          onPress={() => setIsPlaying((prev) => !prev)}
+        />
+        <Button title="Reiniciar" onPress={() => setKey((prev) => prev + 1)} />
+      </View>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingBottom: 100,
+    backgroundColor: "lightyellow",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
+
+export default App;
